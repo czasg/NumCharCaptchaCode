19cd1edeeb6704e57b24e04ea7032337669db7c3
czaorz <czasg0.0>
2020-04-19 21:13:28 +0800

    update

--- model/cnn/NN.py
+++ model/cnn/NN.py
@@ -1,20 +1,23 @@
+# coding: utf-8
 import tensorflow as tf
+
 from model.BaseModel import Model
 
 
 class NN(Model):
+    width = 0
+    height = 0
+    labelLen = 0
 
     def __init__(self):
-        pass
+        self.w = lambda shape: tf.Variable(tf.random.truncated_normal(shape, stddev=0.1))
+        self.b = lambda shape: tf.Variable(tf.constant(0.1, shape=shape))
+        super(NN, self).__init__()
 
-    def add_layer(self, x, inSize, outSize, activeFunc=lambda x: x):
-        """model-layer:
-        y = Wx + b
-        """
-        w = tf.Variable(tf.random_normal([inSize, outSize]))
-        b = tf.Variable(tf.zeros([1, outSize]) + 0.1)
-        y = tf.matmul(x, w) + b
-        return activeFunc(y)
+    def addLayer(self, x, wShape, bShape, *args):
+        w = self.w(wShape)
+        b = self.b(bShape)
+        return self.wxb(w, x, b, *args)
 
     @staticmethod
     def demo():
@@ -26,8 +29,8 @@ class NN(Model):
 
         xs = tf.placeholder(tf.float32, [None, 1])
         ys = tf.placeholder(tf.float32, [None, 1])
-        l1 = nn.add_layer(xs, 1, 10, tf.nn.relu)
-        prediction = nn.add_layer(l1, 10, 1)
+        l1 = nn.addLayer(xs, [1, 10], [1, 10], tf.nn.relu)
+        prediction = nn.addLayer(l1, [10, 1], [1, 1])
         loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
 
         trainStep = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
@@ -35,7 +38,7 @@ class NN(Model):
 
         sess = tf.Session()
         sess.run(init)
-        for _ in range(200):
+        for _ in range(100):
             _, _loss = sess.run([trainStep, loss], feed_dict={
                 xs: x,
                 ys: y
